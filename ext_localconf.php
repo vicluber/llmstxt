156579ac7856704fa23ce1e1f27d0c49ff0e625f
vicluber <victorwillhuber@gmail.com>
2025-08-04 15:15:47 +0200

    Remove LlmsTxt route enhancer

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,3 @@
 <?php
 
 defined('TYPO3') or die();
-
-(static function () {
-    // Register the route enhancer for llms.txt
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['enhancers']['LlmsTxt'] = 
-        \Effective\LlmsTxt\Routing\LlmsTxtEnhancer::class;
-})();
