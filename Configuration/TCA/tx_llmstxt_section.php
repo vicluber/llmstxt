907a4db46c4602e6220175d9546792bca1592866
vicluber <victorwillhuber@gmail.com>
2025-08-04 12:16:58 +0200

    Remove obsolete allowTableOnStandardPages

---
+++ Configuration/TCA/tx_llmstxt_section.php
@@ -0,0 +1,60 @@
+<?php
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:llms_txt/Resources/Private/Language/locallang.xlf:table.section',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'delete' => 'deleted',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+        'searchFields' => 'title',
+        'iconfile' => 'EXT:core/Resources/Public/Icons/T3Icons/svgs/m/apps/pagetree-page.svg',
+    ],
+    'types' => [
+        '1' => ['showitem' => 'title, --div--;LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visibility, hidden, starttime, endtime'],
+    ],
+    'columns' => [
+        'title' => [
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.title',
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'eval' => 'trim,required',
+            ],
+        ],
+        'hidden' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+            ],
+        ],
+        'starttime' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'default' => 0,
+            ],
+        ],
+        'endtime' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'default' => 0,
+            ],
+        ],
+    ],
+];
+
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,18 @@
 CREATE TABLE pages (
-    tx_llmstxt_llms_description text
+    tx_llmstxt_llms_description text,
+    tx_llmstxt_section int DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE tx_llmstxt_section (
+    uid int AUTO_INCREMENT PRIMARY KEY,
+    pid int DEFAULT 0 NOT NULL,
+    tstamp int DEFAULT 0 NOT NULL,
+    crdate int DEFAULT 0 NOT NULL,
+    cruser_id int DEFAULT 0 NOT NULL,
+    deleted tinyint(4) DEFAULT 0 NOT NULL,
+    hidden tinyint(4) DEFAULT 0 NOT NULL,
+    starttime int DEFAULT 0 NOT NULL,
+    endtime int DEFAULT 0 NOT NULL,
+    title varchar(255) DEFAULT '' NOT NULL
 );
 
