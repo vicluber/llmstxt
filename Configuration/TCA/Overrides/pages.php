016a269cb42416b290b50444502ceb5abdddb74b
vicluber <victorwillhuber@gmail.com>
2025-08-04 11:56:26 +0200

    Rename field to LLMs description and adjust tab

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,23 @@
+<?php
+defined('TYPO3') or die();
+
+call_user_func(function () {
+    $newColumns = [
+        'tx_llmstxt_llms_description' => [
+            'label' => 'LLMs description',
+            'description' => 'Description used by LLMs for this page.',
+            'config' => [
+                'type' => 'text',
+                'enableRichtext' => false,
+            ],
+        ],
+    ];
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $newColumns);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'pages',
+        '--div--;LLMO,tx_llmstxt_llms_description',
+        '',
+        'after:seo'
+    );
+});
---
+++ ext_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE pages (
+    tx_llmstxt_llms_description text
+);
+
