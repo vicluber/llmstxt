434461f1459724ec6e7736a3763424caa6505b89
Victor Willhuber <victorwillhuber@gmail.com>
2025-08-04 15:28:58 +0200

    [CLEANING] Adding some data to composer file and cleaning unused controller methods.

--- Classes/Controller/LlmsTxtController.php
+++ Classes/Controller/LlmsTxtController.php
@@ -9,47 +9,5 @@ use Psr\Http\Message\ResponseInterface;
 
 class LlmsTxtController extends ActionController
 {
-    /**
-     * Index action – default entry point
-     *
-     * @return string|ResponseInterface
-     */
-    public function indexAction()
-    {
-        // Example view assignment
-        $this->view->assign('message', 'Hello from LlmsTxtController!');
-        return $this->htmlResponse();
-    }
 
-    /**
-     * Show action example
-     *
-     * @param int $id
-     * @return string|ResponseInterface
-     */
-    public function showAction(int $id)
-    {
-        // Simulate fetching some record based on $id
-        $this->view->assignMultiple([
-            'id' => $id,
-            'content' => 'Simulated content for ID ' . $id,
-        ]);
-        return $this->htmlResponse();
-    }
-
-    /**
-     * Json action example
-     *
-     * @return ResponseInterface
-     */
-    public function jsonAction(): ResponseInterface
-    {
-        $data = [
-            'success' => true,
-            'timestamp' => time(),
-            'message' => 'This is a JSON response.',
-        ];
-
-        return $this->jsonResponse($data);
-    }
 }
